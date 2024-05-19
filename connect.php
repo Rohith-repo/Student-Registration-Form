9cc9e27b1fa8fd00b07d7bff5f576d8495fdc98a
Rohith <133040572+Rohith-repo@users.noreply.github.com>
2024-05-19 11:48:36 +0530

    updated

--- connect.php
+++ connect.php
@@ -1,36 +1,60 @@
 <?php
-// database connection code
-if(isset($_POST['name'])) {
-    // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-    $con = mysqli_connect('localhost', 'root', '741852963','test');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+$con = mysqli_connect($servername, $username, $password, $dbname);
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['name'])) {
+        $name = $_POST['name'];
+        $id = $_POST['id'];
+        $email = $_POST['email'];
+        $dept = $_POST['dept'];
+        $gender = $_POST['gender'];
 
-    // get the post records
-    $name = $_POST['name'];
-    $id = $_POST['id'];
-    $email = $_POST['email'];
-    $dept = $_POST['dept'];
-    $gender = $_POST['gender'];
-
-    // Check if ID already exists
-    $check_query = "SELECT * FROM `registration` WHERE `id` = '$id'";
-    $check_result = mysqli_query($con, $check_query);
-    
-    if(mysqli_num_rows($check_result) > 0) {
-        echo "ID already exists";
-    } else {
-        // database insert SQL code
-        $sql = "INSERT INTO `registration` (`name`, `id`, `email`, `dept`, `gender`) VALUES ('$name', '$id', '$email', '$dept', '$gender')";
-
-        // insert in database 
-        $rs = mysqli_query($con, $sql);
+        $check_query = "SELECT * FROM `registration` WHERE `id` = '$id'";
+        $check_result = mysqli_query($con, $check_query);
         
-        if($rs) {
-            echo "Data Inserted";
+        if (mysqli_num_rows($check_result) > 0) {
+            echo "ID already exists";
         } else {
-            echo "Error inserting records: " . mysqli_error($con);
+            $sql = "INSERT INTO `registration` (`name`, `id`, `email`, `dept`, `gender`) VALUES ('$name', '$id', '$email', '$dept', '$gender')";
+            $rs = mysqli_query($con, $sql);
+            
+            if ($rs) {
+                echo "Data Inserted";
+            } else {
+                echo "Error inserting records: " . mysqli_error($con);
+            }
         }
     }
-} else {
-    echo "Are you a genuine visitor?";
+    if (isset($_POST['show_tables'])) {
+        $sql = "SELECT * FROM `registration`";
+        $result = mysqli_query($con, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            echo "<h2>Data from Table</h2>";
+            echo "<table border='2'>";
+            echo "<tr>";
+            $field_info = mysqli_fetch_fields($result);
+            foreach ($field_info as $field) {
+                echo "<th>{$field->name}</th>";
+            }
+            echo "</tr>";
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                foreach ($row as $data) {
+                    echo "<td>{$data}</td>";
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "0 results";
+        }
+    }
 }
+mysqli_close($con);
 ?>
